45da547a7f6e32b14e07a641b97fa6c5a43740bc
1 <erikamaer00@gmail.com>
2019-01-26 15:51:44 +0300

    change output

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello World! 123ter");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите сообщение:");
+        String line = in.nextLine();
+        System.out.println("Ваше сообщение:");
+        System.out.println(line);
+        in.close();
     }
 }
