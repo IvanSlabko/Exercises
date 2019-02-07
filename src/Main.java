c0da317db02cc8d49ceee81c17dabe800c7043f0
1 <erikamaer00@gmail.com>
2019-02-07 13:58:38 +0300

    second task

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите сообщение:");
-        String line = in.nextLine();
-        System.out.println("Ваше сообщение:"+line);
+        System.out.print("Введите число:");
+        int a = in.nextInt();
+        System.out.print("Введите число:");
+        int b = in.nextInt();
+        System.out.print("Сумма:");
+        System.out.println(a+b);
+
+        System.out.print("Введите число:");
+        a = in.nextInt();
+        System.out.print("Введите число:");
+        b = in.nextInt();
+        System.out.print("Разность:");
+        System.out.println(a-b);
+
+        System.out.print("Введите число:");
+        a = in.nextInt();
+        System.out.print("Введите число:");
+        b = in.nextInt();
+        System.out.print("Произведение:");
+        System.out.println(a*b);
+
+        System.out.print("Введите число:");
+        a = in.nextInt();
+        System.out.print("Введите число:");
+        b = in.nextInt();
+        System.out.print("Частное:");
+        System.out.println(a/b);
         in.close();
     }
 }
