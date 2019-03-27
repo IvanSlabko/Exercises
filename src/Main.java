061789a916297718240b882d39e88b16e5f3bab1
1 <erikamaer00@gmail.com>
2019-03-27 17:36:05 +0300

    fourth task

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите сообщение:");
-        String line = in.nextLine();
-        System.out.println("Ваше сообщение:" + line);
-        System.out.println("Хотите ввести еще? y/n");
-        line = in.nextLine();
-        if (line.equals("y")) {
+        String line;
+        do{
             System.out.println("Введите сообщение:");
             line = in.nextLine();
             System.out.println("Ваше сообщение:" + line);
-        }
+            System.out.println("Хотите ввести еще? y/n");
+            line = in.nextLine();
+        }while (line.equals("y"));
             in.close();
         }
     }
