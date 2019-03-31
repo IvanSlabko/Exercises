a374850326863d3bc609be2512aaa709876f1c29
1 <erikamaer00@gmail.com>
2019-03-31 16:25:26 +0300

    fifth task

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String line;
-        do{
-            System.out.println("Введите сообщение:");
-            line = in.nextLine();
-            System.out.println("Ваше сообщение:" + line);
-            System.out.println("Хотите ввести еще? y/n");
-            line = in.nextLine();
-         }while (line.equals("y"));
+    public class Main {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            String line;
+            int j=0;
+            String[] str = new String[10];
+            do {
+                System.out.println("Введите сообщение:");
+                line = in.nextLine();
+                str[j] = line;
+                for (int i=0; i < str.length; i++) {
+                    System.out.println("Ваше сообщение:" + str[i]);
+                }
+                j++;
+                    System.out.println("Хотите ввести еще? y/n");
+                    line = in.nextLine();
+            } while (line.equals("y")) ;
             in.close();
         }
-    }
+        }
+
+
 
